122a0520229ce380f01da96cf26fe2d3bf71f529
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-06-10 16:06:47 +0200

    finished with index

--- Client/hotels.php
+++ Client/hotels.php
@@ -8,6 +8,37 @@ require('./sections/top.php')
 require('./sections/navigation.php')
 ?>
 
+<!-- Banner Section -->
+<section class="banner" id="banner">
+    <div id="bannerBgAddition" class="banner-addition-bg"></div>
+    <div id="bannerBg" class="banner-bg"></div>
+    <div class="container">
+        <div class="row">
+            <div class="banner-inner" id="bannerInner">
+                <div class="content">
+                    <div class="content-inner">
+                        <h1>
+                            <div class="line">
+                                <span>Our Top Hotels</span>
+                            </div>
+                        </h1>
+                        <p>Is where your vacations starts</p>
+                        <div class="btn-row">
+                            <a href="#">Explore Our top Destinations</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Product Sections -->
+<?php 
+    require('./sections/card.php')
+?>
+<!-- End Of Product Sections -->
+
 <?php
 require('./sections/footer.php');
 ?>
---
+++ Client/sections/banner.php
@@ -0,0 +1,48 @@
+<section class="banner" id="banner">
+    <div id="bannerBgAddition" class="banner-addition-bg"></div>
+    <div id="bannerBg" class="banner-bg"></div>
+    <div class="container">
+        <div class="row">
+            <div class="banner-inner" id="bannerInner">
+                <div class="content">
+                    <div class="content-inner">
+                        <h1>
+                            <div class="line">
+                                <span>Helping clients with</span>
+                            </div>
+                            <div class="line">
+                                <span>their Dream Vacation</span>
+                            </div>
+                        </h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
+                            sunt doloremque repellendus pariatur ad. Necessitatibus
+                            beatae, alias assumenda.
+                        </p>
+                        <div class="btn-row">
+                            <a href="hotels.php">Explore Articles</a>
+                        </div>
+                        <div class="play-video">
+                            <div class="play-video cover"></div>
+                            <span>
+                                <!-- <img src="" alt="play icon" /> -->
+                                Top Vacation Places
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="image">
+                    <div class="image-inner">
+                        <img src="./images/Rectangle.png" alt="">
+                        <!-- <div class="feature-banner not-found" id="featureBanner">
+                                Write to me
+                            </div>
+                            <div class="feature-banner green not-found" id="featureBannerGreen">
+                                All answers here
+                            </div> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
