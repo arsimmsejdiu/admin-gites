8b510bc7213df9a54529f8c449ff0b933d3453b0
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-06-10 11:35:51 +0200

    added product cardds

--- Client/index.php
+++ Client/index.php
@@ -16,9 +16,9 @@
         <div id="header" class="header">
             <form class="nav__search-btn">
                 <button>
-                    <svg>
-                        <use href="./images/icons.svg"></use>
-                    </svg>
+                    <!-- <svg>
+                        <use href="./images/icons.svg#icon-search"></use>
+                    </svg> -->
                 </button>
                 <input type="text" placeholder="Search Article ... " class="nav__search-input" />
             </form>
@@ -68,7 +68,7 @@
                                 <div class="play-video cover"></div>
                                 <span>
                                     <!-- <img src="" alt="play icon" /> -->
-                                    Watch Video
+                                    Top Vacation Places
                                 </span>
                             </div>
                         </div>
@@ -88,9 +88,187 @@
             </div>
         </div>
     </section>
-
     <!-- End of Hero Section -->
-    
+
+    <!-- Product Sections -->
+    <section class="card-section">
+        <div class="container1">
+            <div class="card">
+                <div class="card-header">
+                    <img src="https://c0.wallpaperflare.com/preview/483/210/436/car-green-4x4-jeep.jpg" alt="rover" />
+                </div>
+                <div class="card-body">
+                    <span class="tag tag-teal">San Francisco</span>
+                    <h4>Downtown and modern San Francisco studio apartment</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
+                        sunt doloremque repellendus pariatur ad. Necessitatibus
+                        beatae, alias assumenda.
+                    </p>
+                    <!-- <div class="user">
+                        <img src="https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo" alt="user" />
+                        <div class="user-info">
+                            <h5>July Dec</h5>
+                            <small>2h ago</small>
+                        </div>
+                    </div> -->
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                    <img src="https://www.newsbtc.com/wp-content/uploads/2020/06/mesut-kaya-LcCdl__-kO0-unsplash-scaled.jpg" alt="ballons" />
+                </div>
+                <div class="card-body">
+                    <span class="tag tag-purple">Popular</span>
+                    <h4>
+                        How to Keep Going When You Don’t Know What’s Next
+                    </h4>
+                    <p>
+                        The future can be scary, but there are ways to
+                        deal with that fear.
+                    </p>
+                    <div class="user">
+                        <img src="https://lh3.googleusercontent.com/ogw/ADGmqu8sn9zF15pW59JIYiLgx3PQ3EyZLFp5Zqao906l=s32-c-mo" alt="user" />
+                        <div class="user-info">
+                            <h5>Eyup Ucmaz</h5>
+                            <small>Yesterday</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                    <img src="https://images6.alphacoders.com/312/thumb-1920-312773.jpg" alt="city" />
+                </div>
+                <div class="card-body">
+                    <span class="tag tag-pink">Design</span>
+                    <h4>
+                        10 Rules of Dashboard Design
+                    </h4>
+                    <p>
+                        Dashboard Design Guidelines
+                    </p>
+                    <div class="user">
+                        <img src="https://studyinbaltics.ee/wp-content/uploads/2020/03/3799Ffxy.jpg" alt="user" />
+                        <div class="user-info">
+                            <h5>Carrie Brewer</h5>
+                            <small>1w ago</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                    <img src="https://c0.wallpaperflare.com/preview/483/210/436/car-green-4x4-jeep.jpg" alt="rover" />
+                </div>
+                <div class="card-body">
+                    <span class="tag tag-teal">Technology</span>
+                    <h4>
+                        Why is the Tesla Cybertruck designed the way it
+                        is?
+                    </h4>
+                    <p>
+                        An exploration into the truck's polarising design
+                    </p>
+                    <div class="user">
+                        <img src="https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo" alt="user" />
+                        <div class="user-info">
+                            <h5>July Dec</h5>
+                            <small>2h ago</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                    <img src="https://www.newsbtc.com/wp-content/uploads/2020/06/mesut-kaya-LcCdl__-kO0-unsplash-scaled.jpg" alt="ballons" />
+                </div>
+                <div class="card-body">
+                    <span class="tag tag-purple">Popular</span>
+                    <h4>
+                        How to Keep Going When You Don’t Know What’s Next
+                    </h4>
+                    <p>
+                        The future can be scary, but there are ways to
+                        deal with that fear.
+                    </p>
+                    <div class="user">
+                        <img src="https://lh3.googleusercontent.com/ogw/ADGmqu8sn9zF15pW59JIYiLgx3PQ3EyZLFp5Zqao906l=s32-c-mo" alt="user" />
+                        <div class="user-info">
+                            <h5>Eyup Ucmaz</h5>
+                            <small>Yesterday</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                    <img src="https://c0.wallpaperflare.com/preview/483/210/436/car-green-4x4-jeep.jpg" alt="rover" />
+                </div>
+                <div class="card-body">
+                    <span class="tag tag-teal">Technology</span>
+                    <h4>
+                        Why is the Tesla Cybertruck designed the way it
+                        is?
+                    </h4>
+                    <p>
+                        An exploration into the truck's polarising design
+                    </p>
+                    <div class="user">
+                        <img src="https://yt3.ggpht.com/a/AGF-l7-0J1G0Ue0mcZMw-99kMeVuBmRxiPjyvIYONg=s900-c-k-c0xffffffff-no-rj-mo" alt="user" />
+                        <div class="user-info">
+                            <h5>July Dec</h5>
+                            <small>2h ago</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                    <img src="https://www.newsbtc.com/wp-content/uploads/2020/06/mesut-kaya-LcCdl__-kO0-unsplash-scaled.jpg" alt="ballons" />
+                </div>
+                <div class="card-body">
+                    <span class="tag tag-purple">Popular</span>
+                    <h4>
+                        How to Keep Going When You Don’t Know What’s Next
+                    </h4>
+                    <p>
+                        The future can be scary, but there are ways to
+                        deal with that fear.
+                    </p>
+                    <div class="user">
+                        <img src="https://lh3.googleusercontent.com/ogw/ADGmqu8sn9zF15pW59JIYiLgx3PQ3EyZLFp5Zqao906l=s32-c-mo" alt="user" />
+                        <div class="user-info">
+                            <h5>Eyup Ucmaz</h5>
+                            <small>Yesterday</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                    <img src="https://images6.alphacoders.com/312/thumb-1920-312773.jpg" alt="city" />
+                </div>
+                <div class="card-body">
+                    <span class="tag tag-pink">Design</span>
+                    <h4>
+                        10 Rules of Dashboard Design
+                    </h4>
+                    <p>
+                        Dashboard Design Guidelines
+                    </p>
+                    <div class="user">
+                        <img src="https://studyinbaltics.ee/wp-content/uploads/2020/03/3799Ffxy.jpg" alt="user" />
+                        <div class="user-info">
+                            <h5>Carrie Brewer</h5>
+                            <small>1w ago</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
+    <!-- End Of Product Sections -->
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/animation.gsap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js"></script>
---
+++ Client/sections/footer.php
@@ -0,0 +1,8 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/animation.gsap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/gsap@3.0.1/dist/gsap.min.js"></script>
+<script src="./js/animation.js"></script>
+</body>
+
+</html>
